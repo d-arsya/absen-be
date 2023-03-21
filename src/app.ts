a809efa51304f49491e3f3223390bc7e5673dfe5
Joko Nugroho <jocoding@gmail.com>
2023-03-21 14:06:01 +0700

    listen

--- src/app.ts
+++ src/app.ts
@@ -17,17 +17,17 @@ app.use(cors());
 app.use(morgan('combined'))
 app.use(express.json());
 
+app.use("/api/pegawai", rutePegawai);
+app.use("/api/kehadiran", ruteKehadiran);
+
+ruteWs(app);
+
+const PORT = process.env.PORT || 5000;
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Terhubung dengan MongoDB")
-
-    app.use("/api/pegawai", rutePegawai);
-    app.use("/api/kehadiran", ruteKehadiran);
-
-    ruteWs(app);
-
-    const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
