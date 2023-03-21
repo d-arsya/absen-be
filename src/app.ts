ccd4411a325d0e35094bcb16bfffbf1bfb8f7d26
Joko Nugroho <jocoding@gmail.com>
2023-03-21 14:04:49 +0700

    depend on mongodb

--- src/app.ts
+++ src/app.ts
@@ -19,15 +19,20 @@ app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("Terhubung dengan MongoDB"))
-  .catch((err) => console.log("Gagal terhubung dengan MongoDB:", err ));
+  .then(() => {
+    console.log("Terhubung dengan MongoDB")
 
-app.use("/api/pegawai", rutePegawai);
-app.use("/api/kehadiran", ruteKehadiran);
+    app.use("/api/pegawai", rutePegawai);
+    app.use("/api/kehadiran", ruteKehadiran);
 
-ruteWs(app);
+    ruteWs(app);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Gagal terhubung dengan MongoDB:", err)
 });
+
