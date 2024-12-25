bba02ca87aaa3215be4e44574f6bdc3637715dc0
d-arsya <kamaluddin.arsyad17@gmail.com>
2024-12-26 05:22:37 +0700

    [SOLVED] add health route

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
-import expressWs from 'express-ws';
+import expressWs from "express-ws";
 import cors from "cors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import rutePegawai from "./routes/rutePegawai";
 import ruteKehadiran from "./routes/ruteKehadiran";
-import ruteWs from './routes/ruteWs';
+import ruteWs from "./routes/ruteWs";
 
 dotenv.config();
 
 const app = express();
-expressWs(app)
+expressWs(app);
 
 app.use(cors());
-app.use(morgan('combined'))
+app.use(morgan("combined"));
 app.use(express.json());
-
+app.get("/health", (req, res) => {
+  return res.send(200, { data: "ok" });
+});
 app.use("/api/pegawai", rutePegawai);
 app.use("/api/kehadiran", ruteKehadiran);
 
@@ -27,12 +29,11 @@ const PORT = process.env.PORT || 5000;
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    console.log("Terhubung dengan MongoDB")
+    console.log("Terhubung dengan MongoDB");
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
-    console.log("Gagal terhubung dengan MongoDB:", err)
-});
-
+    console.log("Gagal terhubung dengan MongoDB:", err);
+  });
